9c7c2bdb9a85a6817343252bff69e0e5bd4c12b6
Andrew Ahverdov <wm-andrew@mail.ru>
2023-02-14 16:38:55 +0300

    Добавьте файлы проекта.

---
+++ WpfAppToMyApi/DataBook.cs
@@ -0,0 +1,84 @@
+﻿namespace WpfAppToMyApi
+{
+    public class DataBook
+    {
+        #region Свойства 
+        /// <summary>
+        /// Количество записей.
+        /// </summary>
+        public static int Count { get; set; } = 0;
+
+        /// <summary>
+        /// ID.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Фамилия.
+        /// </summary>
+        public string Surname { get; set; }
+
+        /// <summary>
+        /// Имя.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Отчество.
+        /// </summary>
+        public string MiddleName { get; set; }
+
+        /// <summary>
+        /// Номер телефона.
+        /// </summary>
+        public string TelephoneNumber { get; set; }
+
+        /// <summary>
+        /// Адрес.
+        /// </summary>
+        public string Adress { get; set; }
+
+        /// <summary>
+        /// Описание.
+        /// </summary>
+        public string Note { get; set; }
+        #endregion
+
+        #region Конструкторы
+        /// <summary>
+        /// Конструктор со всеми параметрами.
+        /// </summary>
+        /// <param name="surname"></param>
+        /// <param name="name"></param>
+        /// <param name="middleName"></param>
+        /// <param name="telephoneNumber"></param>
+        /// <param name="adress"></param>
+        public DataBook(string surname, string name, string middleName, string telephoneNumber, string adress, string note)
+        {
+            Count++;
+            //Id = Count;
+            Surname = surname;
+            Name = name;
+            MiddleName = middleName;
+            TelephoneNumber = telephoneNumber;
+            Adress = adress;
+            Note = note;
+        }
+
+        /// <summary>
+        /// Конструктор с автозаполнением.
+        /// </summary>
+        public DataBook()
+        {         
+            Count++;
+            //Id = Count;
+            Surname = $"Тестовая фамилия {Count}";
+            Name = $"Тестовое имя {Count}";
+            MiddleName = $"Тестовое отчество {Count}";
+            TelephoneNumber = $"{Count}{Count}{Count}{Count}{Count}{Count}{Count}{Count}{Count}{Count}{Count}{Count}".Remove(10);
+            Adress = $"Страна {Count}, город {Count}, улица {Count}, дом {Count}";
+            Note = $"Описание для соответсвующего контакта {Count}";
+        }
+        #endregion
+    }
+}
---
+++ WpfAppToMyApi/DataBookDataApi.cs
@@ -0,0 +1,115 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace WpfAppToMyApi
+{
+    public class DataBookDataApi
+    {
+        string baseUrl = @"https://localhost:7065/api/MyApi/";
+        private HttpClient httpClient { get; set; }
+
+        public DataBookDataApi()
+        {
+            httpClient = new HttpClient();
+        }
+
+        /// <summary>
+        /// Получить все записи из БД.
+        /// </summary>
+        /// <returns></returns>
+        public List<DataBook> GetAllDatabooks()
+        {
+            string url = baseUrl;
+
+            string json = httpClient.GetStringAsync(url).Result;
+
+            return JsonConvert.DeserializeObject<List<DataBook>>(json);
+        }
+
+        /// <summary>
+        /// Добавить запись в БД.
+        /// </summary>
+        /// <param name="dataBook">Запись</param>
+        public void CreateDataBook(DataBook dataBook)
+        {
+            string url = baseUrl;
+
+            var r = httpClient.PostAsync(
+                requestUri: url,
+                content: new StringContent(JsonConvert.SerializeObject(dataBook), Encoding.UTF8,
+                mediaType: "application/json")
+                ).Result;
+        }
+
+        /// <summary>
+        /// Получить запись из БД.
+        /// </summary>
+        /// <param name="dataBookId">Id записи</param>
+        /// <returns>Запись</returns>
+        public DataBook ReadDataBook(int dataBookId)
+        {
+            string url = baseUrl + dataBookId;
+
+            string json = httpClient.GetStringAsync(url).Result;
+
+            return JsonConvert.DeserializeObject<DataBook>(json);
+        }
+
+        /// <summary>
+        /// Изменить запсиь в БД.
+        /// </summary>
+        /// <param name="dataBook">Запись</param>
+        public void UpdateDataBook(DataBook dataBook)
+        {
+            UpdateDataBookById(dataBook.Id, dataBook);
+        }
+
+        /// <summary>
+        /// Изменить запсиь в БД по Id.
+        /// </summary>
+        /// <param name="dataBookId">Id записи</param>
+        /// <param name="dataBook">Запись</param>
+        public void UpdateDataBookById(int dataBookId, DataBook dataBook)
+        {
+            string url = baseUrl + dataBookId;
+
+            var dataBookToModify = ReadDataBook(dataBookId);
+
+            if (dataBookToModify != null)
+            {
+                var r = httpClient.PutAsync(
+                    requestUri: url,
+                    content: new StringContent(JsonConvert.SerializeObject(dataBook), Encoding.UTF8,
+                    mediaType: "application/json")
+                    ).Result;
+            }
+        }
+
+        /// <summary>
+        /// Удалить запись из БД.
+        /// </summary>
+        /// <param name="dataBook">Запись</param>
+        public void DeleteDataBook(DataBook dataBook)
+        {
+            DeleteDataBookById(dataBook.Id);
+        }
+
+        /// <summary>
+        /// Удалить запись из БД по Id.
+        /// </summary>
+        /// <param name="dataBookId">Id записи</param>
+        public void DeleteDataBookById(int dataBookId)
+        {
+            string url = baseUrl + dataBookId;
+
+            var dataBookToDelete = ReadDataBook(dataBookId);
+
+            if (dataBookToDelete != null)
+            {
+                var r = httpClient.DeleteAsync(requestUri: url).Result;
+            }
+        }
+    }
+}
